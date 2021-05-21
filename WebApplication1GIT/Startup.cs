477de56cd67c98df91ab75ba83d1306a6149eb84
vks395 <vks395786>
2021-05-21 18:36:26 +0530

    g11

---
+++ WebApplication1GIT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication1GIT.Startup))]
+namespace WebApplication1GIT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
